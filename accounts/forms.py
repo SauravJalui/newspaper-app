56de901481a66b27df3e267d4ab394726a5414dc
SauravJalui <saurav.jalui-codes@outlook.com>
2021-02-10 18:12:05 +0530

    added docstrings

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,11 +3,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    '''This code is to extend the existing UserCreationForm to add a new field'''
     class Meta(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('age',)
     
 class CustomUserChangeForm(UserChangeForm):
+    '''This code is to extend the existing UserChangeForm'''
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
